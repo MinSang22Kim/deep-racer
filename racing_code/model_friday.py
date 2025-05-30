0c21ceafdad6684306fcc01632cde29e7954da34
MinSang22Kim <202014889@kyonggi.ac.kr>
2025-05-30 20:22:21 +0900

    feat: 코너링 out-in-out 보상함수 모델 구현

---
+++ racing_code/model_friday.py
@@ -0,0 +1,41 @@
+def reward_function(params):
+    '''
+    Example of rewarding the agent to follow center line
+    '''
+    reward = 1e-3
+
+    # Read input parameters
+    track_width = params['track_width']
+    distance_from_center = params['distance_from_center']
+    steering_angle = params['steering_angle']
+    speed = params['speed']
+    all_wheels_on_track = params['all_wheels_on_track']
+    is_left_of_center = params['is_left_of_center']
+
+    marker_1 = 0.25 * track_width
+
+    # 1
+    if distance_from_center <= marker_1 and not is_left_of_center:
+        reward *= 1.02
+
+    # 2
+    if steering_angle > 10 and distance_from_center <= marker_1 and is_left_of_center:
+        reward *= 1.03
+
+    # 3
+    if not all_wheels_on_track:
+        return 1e-3
+    else:
+        reward = reward + (params['progress'])
+
+    # 4
+    if speed < 0.70:
+        reward *= 0.90
+    elif speed < 1.30:
+        reward *= 0.80
+    elif speed >= 1.30 and speed <= 2.40:
+        reward += speed
+    else:
+        reward = speed * speed + reward
+
+    return float(reward)
